62dcc7d89a6ffc7441b03a9732cab256a9afcde7
Michael Ho <michaelho3624@gmail.com>
2023-03-26 18:13:09 -0400

    Add property test for update

--- test/room_here/listings/property_test.exs
+++ test/room_here/listings/property_test.exs
@@ -8,12 +8,31 @@ defmodule RoomHere.Listings.PropertyTest do
       :ok = Ecto.Adapters.SQL.Sandbox.checkout(RoomHere.Repo)
     end
 
-    test "changeset success" do
+    test "changeset success on create" do
       params = RoomHere.Factory.params_for(:property)
 
       changeset = Property.changeset(%Property{}, params)
 
       assert changeset.valid? == true
+    end
+
+    test "changeset success on update" do
+      property = RoomHere.Factory.insert(:property)
+
+      params = %{
+        title: "Swab the poop deck!",
+        minimum_term: property.minimum_term,
+        maximum_term: property.maximum_term,
+        description: property.description,
+        first_available_date: property.first_available_date
+      }
+
+      new_property =
+        property
+        |> Property.changeset(params)
+        |> RoomHere.Repo.update!()
+
+      assert new_property.title == "Swab the poop deck!"
     end
 
     test "Changeset failure on validation" do
