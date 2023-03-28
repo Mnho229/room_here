9ec5b1e07c93bba1d4b8e048f40391d196c4bfcb
Michael Ho <michaelho3624@gmail.com>
2023-03-27 20:56:41 -0400

    Add tests for property user changeset

--- test/room_here/property_user_test.exs
+++ test/room_here/property_user_test.exs
@@ -48,5 +48,50 @@ defmodule RoomHere.PropertyUserTest do
 
       assert changeset_errors.user_id == ["can't be blank"]
     end
+
+    test "changeset failure on unique constraint for user and property", %{
+      property: property,
+      user: user
+    } do
+      property_user =
+        RoomHere.Factory.insert(:property_user_primary, property: property, user: user)
+
+      params = %{
+        is_primary_user: false,
+        property_id: property_user.property_id,
+        user_id: property_user.user_id
+      }
+
+      changeset_errors =
+        %PropertyUser{}
+        |> PropertyUser.changeset(params)
+        |> RoomHere.Repo.insert()
+        |> elem(1)
+        |> errors_on()
+
+      assert changeset_errors.user_id == ["has already been taken"]
+    end
+
+    test "changeset failure on unique constraint for is_primary_user and property", %{
+      property: property,
+      user: user
+    } do
+      property_user = RoomHere.Factory.insert(:property_user_primary, property: property)
+
+      params = %{
+        is_primary_user: true,
+        property_id: property_user.property_id,
+        user_id: user.id
+      }
+
+      changeset_errors =
+        %PropertyUser{}
+        |> PropertyUser.changeset(params)
+        |> RoomHere.Repo.insert()
+        |> elem(1)
+        |> errors_on()
+
+      assert changeset_errors[:is_primary_user] == ["has already been taken"]
+    end
   end
 end
